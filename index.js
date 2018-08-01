dd1f4820853be5eb1dfe36bb1163fb1bb93a88cf
Albert Chun <albert.s.chun@gmail.com>
2018-08-01 12:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function subtract(a, b) {
   return a - b
 }
 
-function multiple(a, b) {
+function multiply(a, b) {
   return a * b
 }
 
-function divive(a, b) {
+function divide(a, b) {
   return a / b
 }
 
